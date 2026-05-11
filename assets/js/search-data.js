da21036fd00aed95c63a3de419825ff24929fca4
riccardopaoli <116311171+riccardopaoli@users.noreply.github.com>
2026-05-11 13:44:45 +0000

    Deploying to gh-pages from @ riccardopaoli/riccardopaoli.github.io@171d5c22300ce05e04fdff9992299f7e37148e9f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,35 +469,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("mailto:%72%69%63%63%61%72%64%6F.%70%61%6F%6C%69@%69%73%74%69.%63%6E%72.%69%74", "_blank");
         },
       },{
       id: 'light-theme',
