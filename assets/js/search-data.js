76e91622a953fc8a47c8167d4fbed6a60798e4ce
riccardopaoli <116311171+riccardopaoli@users.noreply.github.com>
2026-05-12 07:03:11 +0000

    Deploying to gh-pages from @ riccardopaoli/riccardopaoli.github.io@50ffa5f7f06851c33dc64dc35184d8185328abf8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "A summary of my academic background and research experience. For the complete overview, [download my full CV (PDF)](/assets/pdf/curriculum.pdf).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
