5ab5ed7cd532272045bd944ceeb92686525bd24c
riccardopaoli <116311171+riccardopaoli@users.noreply.github.com>
2026-08-14 19:22:44 +0000

    Deploying to gh-pages from @ riccardopaoli/riccardopaoli.github.io@4f5bcad43ec7b0bfc3fb05637b8de9573951f090 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,7 +409,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "teachings-data-science-fundamentals",
+          section: "News",},{id: "projects-tesi-triennale",
+          title: 'Tesi Triennale',
+          description: "Il Teorema di Stokes In Relatività Generale (Only in Italian)",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/tesi_triennale/";
+            },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
           section: "Teachings",handler: () => {
